06d269e6b8ae81c6d5aa2066fa5203ecaa34ef29
Alek Korzeniowski <akorze@udel.edu>
2018-04-05 10:40:26 -0400

    JS Exercise 2

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,11 @@
+blist = ["Car","House","Dog"];
+blist.push("Cat");
+console.log(blist.length);
+console.log(blist);
+
+/*For loop
+below
+*/
+for (i=0; i< blist.length; i++){
+  console.log(blist[i]);
+}

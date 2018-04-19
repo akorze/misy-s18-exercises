ae0b1ce332ebc11173fb0e92c2303263b6da61ad
Alek Korzeniowski <akorze@udel.edu>
2018-04-19 10:26:56 -0400

    add exercises

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+#this is my first python code
+greeting = "Hello"
+name = "Alek"
+print greeting  #this prints out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %d! How are you doing today?" % (greeting, 7)

43ef16d1585a3bea9a132e34f074333a0a3411c7
Alek Korzeniowski <akorze@udel.edu>
2018-05-01 10:38:04 -0400

    added templates

--- songbase2/songbase2.py
+++ songbase2/songbase2.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "hello world"
+    #return "hello world"
+    return render_template('index.html')
+
+@app.route('/users/<string:username>')
+def users(username):
+    #return "<h1>Hello %s</h1>" % username
+    return render_template('user.html', uname=username)
 
 
 @app.route('/user')
